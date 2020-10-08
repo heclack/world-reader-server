8bf2b6c42b7e5b0212797a48f8ce280650057c83
heclack <elizaclack@gmail.com>
2020-10-08 14:39:45 -0700

    refactor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require( 'express' );
 const fileUpload = require( 'express-fileupload' );
+const {spawn} = require('child_process');
+
 const classyImage = require('./controllers/classyImage');
 
-const net = require('net');
 const crypto = require('crypto');
 const fs = require( 'fs' );
 
@@ -11,35 +12,23 @@ const app = express();
 app.use( express.static( './public' ) );
 app.use( fileUpload() );
 
-
-app.get( '/capture/:cid', ( req, res ) =>{
-  let captureFile = fs.readFileSync( `./tmp/capture${cid}.jpg` );
-  res.sendFile( captureFile );
-});
-
-app.get( '/deps/:name', ( req, res ) =>{
-  let depend = fs.readFileSync( `./models/${req.params.name}.js` );
-  res.sendFile( depend );
-});
-
-
-
 app.post( '/upload', ( req, res ) =>{
-  if (!req.files || Object.keys( req.files ).length === 0){
-    return res.status( 400 ).send( 'No files were uploaded.' );
-  };
-  let upload = req.files.capture_image;
-  let captureResults = {
-    cid: crypto.createHash('md5').update(`${upload}`).digest('hex')
-  };
-  upload.mv( `./tmp/capture${captureResults.cid}.jpg`, (err)=>{
-      if(err) res.status(500).send('file moved')
-   });
-   classyImage(`./tmp/capture${captureResults.cid}.jpg`).then((results)=>{
-     captureResults.res = results;
-     console.log(results);
-     
-   }).catch(err=>{console.log(err)});
+  if(!req.files.capture_image){
+    return res.status(400)
+  } else {
+    (async(file = req.files.capture_image)=>{
+        let filename = `./tmp/capt_${file.md5}.${file.mimetype.split('/')[1]}`;
+        console.log(filename);
+        file.mv(filename, (err)=>{
+          if (err){
+            return console.log(err)
+          };
+        })
+      classyImage(filename).then((results)=>{
+        console.log(results)
+      }).catch(err=>{console.log(err)});
+    })();
+  }
 });
 
 
